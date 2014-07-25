ce41e47a532a4ff3c82db90a5b2d571433199e66
jules testard <jules.testard@gmail.com>
2014-07-24 17:45:51 -0700

    ybeacon web app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Beacons
+beacon1 = Beacon.create(major: 0, minor: 0)
+beacon2 = Beacon.create(major: 0, minor: 1)
+
+Behavior.create(beacon_id: beacon1.id, proximity: 'near', status: 'entering', action: 'http://www.yelp.com')
+Behavior.create(beacon_id: beacon1.id, proximity: 'immediate', status: 'entering', action: 'http://www.yelp.com')
+Behavior.create(beacon_id: beacon2.id, proximity: 'near', status: 'entering', action: 'http://www.yelp.com')
