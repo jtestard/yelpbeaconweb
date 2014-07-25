66cfa5cbc2086c7f71cf45fe256742f658cb9d9a
Jules Testard <jtestard@140-sfoeng50-41.clients.corp.yelpcorp.com>
2014-07-24 21:48:18 -0700

    minor

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@
 password = 'favourite_password'
 password_salt = BCrypt::Engine.generate_salt()
 password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-business = Business.create(yelp_username: 'jtestard', password_salt: )
+business = Business.create(yelp_username: 'jtestard', password_hash: password_hash, password_salt: password_salt)
 
 #Beacons
-beacon1 = Beacon.create(uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', major: 0, minor: 4)
-beacon2 = Beacon.create(uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', major: 0, minor: 1)
+beacon1 = Beacon.create(business_id: business.id, uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', major: 0, minor: 4)
+beacon2 = Beacon.create(business_id: business.id, uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', major: 0, minor: 1)
 
 #Behaviors
 Behavior.create(beacon_id: beacon1.id, proximity: 'immediate', status: 'inside', action: 'yelp:///biz/add/check_in?biz_id=xsgVQ7QMslfYD4E8xQ7GNQ')
